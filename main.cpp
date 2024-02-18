297f96e355d81fa5e07c5b16d331633885e0286f
jalburez09 <160087756+jalburez09@users.noreply.github.com>
2024-02-18 11:08:10 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int numero = 6;
-    int r = 1;
-    cout << "tabla de multiplicar del " << numero <<  " : " << endl;
-    do  {
-        int multiplicacion = numero * r;
-        cout << numero << " x " << r << " = " << multiplicacion << endl;
-        r++;
-        } while ( r <= 10 );
+    int numero;
+    int a = 1;
 
+    // Tabla del 4
+    cout << "Tabla del 4:" << endl;
+    numero = 4;
+    do {
+        int multiplicacion = numero * a;
+        cout << numero << " x " << a << " = " << multiplicacion << endl;
+        a++;
+    } while (a <= 10);
 
+    // Tabla del 6
+    cout << "\nTabla del 6:" << endl;
+    numero = 6;
+    a = 1;
+    do {
+        int multiplicacion = numero * a;
+        cout << numero << " x " << a << " = " << multiplicacion << endl;
+        a++;
+    } while (a <= 10);
+
+    
     return 0;
 }
