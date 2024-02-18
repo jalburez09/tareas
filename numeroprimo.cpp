17d42920c2c4679de236e08823ae2409c6c3f289
jalburez09 <160087756+jalburez09@users.noreply.github.com>
2024-02-18 11:37:18 -0600

    Add files via upload

---
+++ numeroprimo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+bool esPrimo(int numero) {
+    if (numero <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= numero; ++i) {
+        if (numero % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int numero;
+    std::cout << "Ingrese un número: ";
+    std::cin >> numero;
+
+    if (esPrimo(numero)) {
+        std::cout << "El número ingresado es primo." << std::endl;
+    } else {
+        std::cout << "El número ingresado NO es primo." << std::endl;
+    }
+
+    return 0;
+}
